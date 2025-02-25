8cb95debe000e53f79359c684ae3313099909e22
Zhengyang_Li <zodooy@gmail.com>
2025-02-25 23:21:18 +0800

    optimize

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from utils.data import*
 if __name__ == "__main__":
     # run_list = ["inclusion_layer", "inclusion", "four_layer", "two_layer",
     #     "checker2", "checker8", "1420", "1465", "1480", "1510", "1540", "1555", "1570"]
-    run_list = ["inclusion_layer"]
+    run_list = ["two_layer"]
 
     # Run all examples
     for sample in run_list:
